67338dc697dc7110051f2442191d850c00127dc9
astriktorres <astriktorres@gmail.com>
2022-05-27 21:57:54 -0500

    Alias con WebPack y correcion de imagenes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import '../styles/Header.scss';
-import logo from '../../public/assets/logos/logo_yard_sale.svg';
-import iconMenu from '../../public/assets/icons/icon_menu.svg';
-import iconCart from '../../public/assets/icons/icon_shopping_cart.svg';
+import '@styles/Header.scss';
+import logo from '@logos/logo_yard_sale.svg';
+import iconMenu from '@icons/icon_menu.svg';
+import iconCart from '@icons/icon_shopping_cart.svg';
 
 const Header = () => {
   return (
     <nav>
       <img src={iconMenu} alt="menu" className="menu" />
       <div className="navbar-left">
-        <img src={logo} alt="logo" className="logo" />
+        <img src={logo} alt="logo" className="nav-logo" />
         <ul>
           <li>
             <a href="/">All</a>
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
-import Layout from '../containers/Layout';
-import Login from  '../pages/Login';
-import RecoveryPassword from '../pages/RecoveryPassword';
-import Home from '../pages/Home';
-import NotFound from '../pages/NotFound';
-import SendEmail from '../pages/SendEmail';
-import NewPassword from '../pages/NewPassword';
-import MyAccount from '../pages/MyAccount';
-import CreateAccount from '../pages/CreateAccount';
-import Checkout from '../pages/Checkout';
-import Orders from '../pages/Orders';
-import '../styles/global.scss';
+import Layout from '@containers/Layout';
+import Login from  '@pages/Login';
+import RecoveryPassword from '@pages/RecoveryPassword';
+import Home from '@pages/Home';
+import NotFound from '@pages/NotFound';
+import SendEmail from '@pages/SendEmail';
+import NewPassword from '@pages/NewPassword';
+import MyAccount from '@pages/MyAccount';
+import CreateAccount from '@pages/CreateAccount';
+import Checkout from '@pages/Checkout';
+import Orders from '@pages/Orders';
+import '@styles/global.scss';
 
 export const App = () => {
   return (
